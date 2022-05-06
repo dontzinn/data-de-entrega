fea8ccd66453a19a01d60a80046ab28ae13c5cba
Pedro <hardpvloksluck@yahoo.com.br>
2022-05-05 22:34:18 -0300

    Primeira alteração

---
+++ entrega.py
@@ -0,0 +1,108 @@
+b = input()
+n = int(input())
+
+if b == 'domingo' and n == 0:
+    print('chega hoje!')
+elif b == 'domingo' and n ==1:
+    print('sera entrega segunda')
+elif b == 'domingo' and n ==2:
+    print('sera entregue terca')
+elif b == 'domingo' and n ==3:
+    print('sera entregue quarta')
+elif b == 'domingo' and n ==4:
+    print('sera entregue quinta')
+elif b == 'domingo' and n ==5:
+    print('sera entregue sexta')
+elif b == 'domingo' and n ==6:
+    print('sera entregue sábado')
+
+elif b == 'segunda' and n ==0:
+    print('chega hoje!')
+elif b == 'segunda' and n ==1:
+    print('sera entregue terca')
+elif b == 'segunda' and n ==2:
+    print('sera entregue quarta')
+elif b == 'segunda' and n ==3:
+    print('sera entregue quinta')
+elif b == 'segunda' and n ==4:
+    print('sera entregue sexta')
+elif b == 'segunda' and n ==5:
+    print('sera entregue sabado')
+elif b == 'segunda' and n ==6:
+    print('sera entregue domingo')
+
+elif b == 'terca' and n ==0:
+    print('chega hoje!')
+elif b == 'terca' and n ==1:
+    print('sera entregue quarta')
+elif b == 'terca' and n ==2:
+    print('sera entregue quinta')
+elif b == 'terca' and n ==3:
+    print('sera entregue sexta')
+elif b == 'terca' and n ==4:
+    print('sera entregue sabado')
+elif b == 'terca' and n ==5:
+    print('sera entregue domingo')
+elif b == 'terca' and n ==6:
+    print('sera entregue sabado')
+    
+elif b == 'quarta' and n ==0:
+    print('chega hoje!')
+elif b == 'quarta' and n ==1:
+    print('sera entregue quinta')
+elif b == 'quarta' and n ==2:
+    print('sera entregue sexta')
+elif b == 'quarta' and n ==3:
+    print('sera entregue sabado')
+elif b == 'quarta' and n ==4:
+    print('sera entregue domingo')
+elif b == 'quarta' and n ==5:
+    print('sera entregue segunda')
+elif b == 'quarta' and n ==6:
+    print('sera entregue terca')
+    
+elif b == 'quinta' and n ==0:
+    print('chega hoje!')
+elif b == 'quinta' and n ==1:
+    print('sera entregue sexta')
+elif b == 'quinta' and n ==2:
+    print('sera entregue sabado')
+elif b == 'quinta' and n ==3:
+    print('sera entregue segunda')
+elif b == 'quinta' and n ==4:
+    print('sera entregue terca')
+elif b == 'quinta' and n ==5:
+    print('sera entregue quarta')
+elif b == 'quinta' and n ==6:
+    print('sera entregue quinta')
+
+elif b == 'sexta' and n ==0:
+    print('chega hoje!')
+elif b == 'sexta' and n ==1:
+    print('sera entregue sabado')
+elif b == 'sexta' and n ==2:
+    print('sera entregue domingo')
+elif b == 'sexta' and n ==3:
+    print('sera entregue segunda')
+elif b == 'sexta' and n ==4:
+    print('sera entregue terca')
+elif b == 'sexta' and n ==5:
+    print('sera entregue quarta')
+elif b == 'sexta' and n ==6:
+    print('sera entregue quinta')
+    
+elif b == 'sabado' and n == 0:
+    print('chega hoje!')
+elif b == 'sabado' and n == 1:
+    print('sera entregue domingo')
+elif b == 'sabado' and n == 2:
+    print('sera entregue segunda')
+elif b == 'sabado' and n == 3:
+    print('sera entregue terca')
+elif b == 'sabado' and n == 4:
+    print('sera entregue quarta')
+elif b == 'sabado' and n == 5:
+    print('sera entregue quinta')
+elif b == 'sabado' and n == 6:
+    print('sera entregue sexta')
+
